fe8beb4b0ba78cfe9f387b63ff49b137c73a3dac
Glauber Ramos <glauberamos@gmail.com>
2012-06-24 21:23:29 -0300

    adding smoothly reset

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,35 @@
 carrosseljs.main = function() {
   var slidesController = new carrosseljs.SlidesController(7, 250);
+  var resetIntervalId;
 
   function autoSlide() {
     var position = document.getElementById('carrossel-list').style.left;
-    var nextPosition;
     var numberedPosition = position.substring(0, position.length - 2);
+    var nextPosition;
 
     if(numberedPosition != slidesController.getLastPositionViewPort()) {
       nextPosition = numberedPosition - 1;
     } else {
+      resetIntervalId = window.setInterval(smoothlyReset, 1);
+    }
+    
+    document.getElementById('carrossel-list').style.left = nextPosition + 'px';
+  };
+
+  function smoothlyReset() {
+    var position = document.getElementById('carrossel-list').style.left;
+    var numberedPosition = position.substring(0, position.length - 2);
+    var nextPosition;
+
+    if(numberedPosition < 0) {
+      nextPosition = parseFloat(numberedPosition) + 10;
+    } else {
+      clearInterval(resetIntervalId);
       nextPosition = '0';
     }
+
     document.getElementById('carrossel-list').style.left = nextPosition + 'px';
-  }
+  };
 
   window.onload = function() {
       var setIntervalId = window.setInterval(autoSlide, 10);
